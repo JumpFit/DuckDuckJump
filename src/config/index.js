e7af22b89b86d0ceb173f8644b6325f22e34eabe
Anthony Liang <antliang1117@gmail.com>
2022-01-21 16:21:28 -0500

    style: add backgroundColor constant to config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
+const { BACKGROUND_COLOR } = require('../constants');
+
 export default {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   parent: 'game',
-  backgroundColor: '#5DACD8',
+  backgroundColor: BACKGROUND_COLOR,
   dom: {
     createContainer: true,
   },
