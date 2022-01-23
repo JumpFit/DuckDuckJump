5126b42d474612aa507c0eeaff574d0983dbf5ed
Greg Solomon <ghsolomon@gmail.com>
2022-01-23 16:42:36 -0500

    chore: add secure route for user to get game history

--- server/api/index.js
+++ server/api/index.js
@@ -2,3 +2,4 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use('/games', require('./games'));
+router.use('/users', require('./users'));
---
+++ server/api/users.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const {
+  models: { User, Game },
+} = require('../db');
+module.exports = router;
+
+const authenticateUser = async (req, res, next) => {
+  try {
+    req.user = await User.findByToken(req.headers.authorization);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+// router.put('', authenticateUser, (req, res, next) => {
+//   try{
+//     if (req.user)
+//   }
+// })
+
+// GET /api/users/:userId/games
+router.get('/:username/games', authenticateUser, async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    if (req.user.username !== username) {
+      const error = new Error('Insufficient permission to view user data');
+      error.status = 401;
+      next(error);
+    } else {
+      res.json(req.user.games);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
