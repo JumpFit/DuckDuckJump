8bc0a0db9d7d5843bb7ce1ff2641f04b1dfab669
Anthony Liang <antliang1117@gmail.com>
2022-01-22 22:51:48 -0500

    feat: incorporate functionality for main menu

--- src/scenes/MainMenuScene.js
+++ src/scenes/MainMenuScene.js
@@ -13,14 +13,68 @@ export default class MainMenuScene extends Phaser.Scene {
     );
     this.load.image('title', 'assets/mainmenu/title.png');
     this.load.image('ducks', 'assets/mainmenu/ducksinthemiddle.png');
+    this.load.image('login', 'assets/mainmenu/login.png');
+    this.load.image('signup', 'assets/mainmenu/signup.png');
+    this.load.image('howtoplay', 'assets/mainmenu/howtoplay.png');
+    this.load.image('leaderboard', 'assets/mainmenu/leaderboard.png');
+    this.load.image('startgame', 'assets/mainmenu/startgame.png');
   }
 
   create() {
     const scene = this;
 
-    scene.add.image(0, 0, 'backgroundImage').setOrigin(0);
-    scene.add.image(-200, 0, 'title').setOrigin(0);
-    scene.add.image(-200, 0, 'ducks').setOrigin(0);
+    scene.add.image(0, 0, 'backgroundImage').setOrigin(0).setDepth(0);
+    scene.add.image(0, 0, 'title').setOrigin(0).setDepth(1);
+    scene.add.image(0, 0, 'ducks').setOrigin(0).setDepth(1);
+
+    const loginButton = scene.add
+      .image(28, 55, 'login')
+      .setOrigin(0)
+      .setDepth(2)
+      .setInteractive({ userHandCursor: true });
+    loginButton.on('pointerdown', function () {
+      scene.scene.stop('MainMenuScene');
+      scene.scene.start('LoginScene');
+    });
+
+    const signupButton = scene.add
+      .image(1393, 60, 'signup')
+      .setOrigin(0)
+      .setDepth(2)
+      .setInteractive({ userHandCursor: true });
+    signupButton.on('pointerdown', function () {
+      scene.scene.stop('MainMenuScene');
+      scene.scene.start('SignupScene');
+    });
+
+    const howToPlayButton = scene.add
+      .image(40, 780, 'howtoplay')
+      .setOrigin(0)
+      .setDepth(2)
+      .setInteractive({ userHandCursor: true });
+    howToPlayButton.on('pointerdown', function () {
+      scene.scene.stop('MainMenuScene');
+      scene.scene.start('PlayScene');
+    });
+
+    const leaderboardButton = scene.add
+      .image(1257, 785, 'leaderboard')
+      .setOrigin(0)
+      .setDepth(2)
+      .setInteractive({ userHandCursor: true });
+    leaderboardButton.on('pointerdown', function () {});
+
+    const startGameButton = scene.add
+      .image(538, 720, 'startgame')
+      .setOrigin(0)
+      .setDepth(2)
+      .setInteractive({ userHandCursor: true });
+    startGameButton.on('pointerdown', function () {
+      scene.scene.stop('MainMenuScene');
+
+      // starts PlayScene but will need to change later to main game
+      scene.scene.start('PlayScene');
+    });
   }
 
   update() {}
